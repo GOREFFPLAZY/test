83a66d4056e5cae72a806dcf58c57b443192bd8c
sirbiggies <neo.varnhed@gmail.com>
2025-05-11 00:41:42 +0200

    Update panic.js

--- panic.js
+++ panic.js
@@ -1,7 +1,7 @@
 // Panic Key Script
 document.addEventListener('keydown', function(event) {
     // Check if the Escape key (key code 27) is pressed
-    if (event.key === 'Quote') {
+    if (event.key === 'Escape') {
         // Redirect to a safe or neutral webpage
         window.location.href = 'https://www.google.com/'; // Replace with your desired URL
     }
